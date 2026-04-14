ccc68764d7fa7f175eeeee3d4dfd8b18fc3de534
Zaf0708 <zafroollah@gmail.com>
2026-04-14 23:54:00 +1000

    Use Inter as default font style.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
             alt="Frontend Developer Logo"
             width={120}
             height={40}
-            className="h-14 w-auto object-contain brightness-0 invert opacity-90"
+            className="h-10 w-auto object-contain brightness-0 invert opacity-90"
             priority
           />
         </Link>
