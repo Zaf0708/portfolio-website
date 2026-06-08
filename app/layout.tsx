93afd6eba6a3289b96f467b2ba24d45b646cbe54
Zaf0708 <zafroollah@gmail.com>
2026-06-08 22:52:28 +1000

    Install vercel analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 
@@ -25,6 +26,7 @@ export default function RootLayout({
     >
       <body className="bg-[var(--brand-white)] text-[var(--brand-text)] antialiased font-sans">
         {children}
+        <Analytics />
       </body>
     </html>
   );
