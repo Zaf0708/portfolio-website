fc3b9eaf6207437b83562368a1cacb7b4e72eb52
Zaf0708 <zafroollah@gmail.com>
2026-06-21 00:32:05 +1000

    fix: regenerate corrupted package-lock.json

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = nextConfig;
 
 // Injected content via Sentry wizard below
 
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(module.exports, {
